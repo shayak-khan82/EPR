394d0fa9052a3639c0c39ccdaf8b35ccd316c5cf
shayake <shayakkhan82@gmail.com>
2026-04-14 15:21:14 +0530

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/dashboard',
+  assetPrefix: '/dashboard',
+};
 
-export default nextConfig;
+module.exports = nextConfig;gi
